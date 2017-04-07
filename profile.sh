ae2b93e26a4648c2a2a490423d1a049055c69c2c
CircleCI <circleci@circleci>
2017-04-07 10:19:27 +0200

    Fixed profile scrip

--- profile.sh
+++ profile.sh
@@ -1,7 +1,15 @@
 mkdir -p out
 mkdir -p recording
 
-hash perf 2>/dev/null || { echo >&2 "I require perf but it's not installed.  Aborting."; exit 1; }
+hash perf 2>/dev/null || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+
+perf record  -a -- sleep 1 1>/dev/null 2>/dev/null
+if [ $? -eq 0 ]; then
+    echo ""
+else
+    echo "perf couldn't execute due to permission failures, run the allow_perf.sh script and try again or run perf record manually and check for errors"
+    exit 1;
+fi
 
 while true
 do
@@ -9,3 +17,4 @@ do
   perf record -F 505 -a -g -o recording/$out_file_name -- sleep 30
   mv recording/$out_file_name out
 done
+
