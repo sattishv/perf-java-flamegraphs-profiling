5e1a1d9fc45f3b9edb77ea906e331f7e7a14d1a0
CircleCI <circleci@circleci>
2017-04-08 13:29:52 +0200

    Improved draw.sh script

--- draw.sh
+++ draw.sh
@@ -1,4 +1,5 @@
-TIME_START=1491650215
-TIME_END=1491650518
+TIME_START="${1:-1491650215}"
+TIME_END="${2:-1491650518}"
+
 rm ./flamegraph.svg
 python ./scripts/mongodb-to-jsons.py $TIME_START $TIME_END | ./scripts/jsons-to-stacks.py | ./FlameGraph/flamegraph.pl --color=java > flamegraph.svg
