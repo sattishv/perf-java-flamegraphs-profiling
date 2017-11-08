06c941bb7ce36ac15cbd7eeaff19bb4970f2f509
CircleCI <circleci@circleci>
2017-11-08 16:38:31 +0100

    Removed the settis used for timestamping and reorganized a little bit everything.

--- mongodb/eve/settings.py
+++ mongodb/eve/settings.py
@@ -1,8 +1,3 @@
-# This file contains the data schema used in the settings.py file
-# that the EVE framework uses to configure mongodb domains
-
-# Add this content accordingly
-
 cpu_schema = {
     # Schema definition, based on Cerberus grammar. Check the Cerberus project
     # (https://github.com/pyeve/cerberus) for details.
@@ -44,4 +39,29 @@ cpu = {
     'schema': cpu_schema
 }
 
+
 DOMAIN = {'cpu': cpu}
+
+# Let's just use the local mongod instance. Edit as needed.
+
+# Please note that MONGO_HOST and MONGO_PORT could very well be left
+# out as they already default to a bare bones local 'mongod' instance.
+MONGO_HOST = "192.168.56.111"
+MONGO_PORT = 27017
+
+# Skip these if your db has no auth. But it really should.
+#MONGO_USERNAME = '<your username>'
+#MONGO_PASSWORD = '<your password>'
+
+MONGO_DBNAME = 'profiling'
+# Enable reads (GET), inserts (POST) and DELETE for resources/collections
+# (if you omit this line, the API will default to ['GET'] and provide
+# read-only access to the endpoint).
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+
+# Enable reads (GET), edits (PATCH), replacements (PUT) and deletes of
+# individual items  (defaults to read-only item access).
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+
+
+X_DOMAINS = ['*']
